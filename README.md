b756fedd460cd961c11e9c0deeb8a99a66baffaa
davidrocha9 <up201806371@fe.up.pt>
2021-11-17 11:19:48 +0000

    Preprocessing data in a better way

---
+++ README.md
@@ -0,0 +1,9 @@
+03/11/2021 - First attempt of undersampling for model training, had horrible results
+
+04/11/2021 - Tweaked some details on the undersampling technique, slightly improvements
+
+05/11/2021 - Started checking for outliers, no improvements whatsoever
+
+07/11/2021 - Changed used columns based on correlation matrix, didn't improve
+
+09/11/2021 - Changed the algorithm used from SVM to Neural Networks, had significant improvement
--- test.py
+++ test.py
@@ -14,13 +14,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.svm import SVC
-from sklearn.metrics import classification_report, accuracy_score
-from sklearn.metrics import roc_curve
-from sklearn.metrics import roc_auc_score
-from matplotlib import pyplot
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import make_scorer
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+from sklearn.ensemble import AdaBoostClassifier
 
 ### Cleaning Data
 
@@ -35,6 +30,7 @@ trans_train_data = pd.read_csv('data/trans_train.csv', na_values=['NA'], sep=';'
 card_test_data = pd.read_csv('data/card_test.csv', na_values=['NA'], sep=';', low_memory=False)
 loan_test_data = pd.read_csv('data/loan_test.csv', na_values=['NA'], sep=';', low_memory=False)
 trans_test_data = pd.read_csv('data/trans_test.csv', na_values=['NA'], sep=';', low_memory=False)
+good = pd.read_csv('good.csv', na_values=['NA'], sep=',', low_memory=False)
 
 # Removing unnecessary columns
 account_data = account_data.drop(columns=["date"])
@@ -63,11 +59,10 @@ test_data = test_data.merge(client_data, on='client_id', how='inner')
 # Removing NaN values
 
 col_list = []
-ignoredCols = ['name ', 'region', 'card_id', 'type_x', 'issued', 'district_id_x', 'client_id',
-                'district_id_y', 'disp_id', 'account_id', 'trans_id', 'operation', 'bank', 'type',
-                'k_symbol', 'account', 'no. of municipalities with inhabitants < 499 ',
-                'no. of municipalities with inhabitants 2000-9999 ', 'average salary ',
-                "unemploymant rate '95 ", "no. of commited crimes '95 "]
+ignoredCols = ['account_id', 'code', 'disp_id', 'client_id', 'card_id', 'account', 'bank',
+                "unemploymant rate '95 ", "no. of commited crimes '95 ", 'issued',
+                'name ', 'operation', 'type_y', 'region', 'type', 'trans_id',
+                'district_id_x', 'district_id_y']
 
 for key in train_data.keys():
     if key not in ignoredCols:
@@ -76,46 +71,63 @@ for key in train_data.keys():
 train_data = train_data[col_list]
 test_data = test_data[col_list]
 
-# train_data["unemploymant rate '95 "] = pd.to_numeric(train_data["unemploymant rate '95 "], errors='coerce')
-# train_data["no. of commited crimes '95 "] = pd.to_numeric(train_data["no. of commited crimes '95 "], errors='coerce')
-# train_data["unemploymant rate '95 "] = pd.to_numeric(train_data["unemploymant rate '95 "], errors='coerce')
-# train_data["no. of commited crimes '95 "] = pd.to_numeric(train_data["no. of commited crimes '95 "], errors='coerce')
+type_x_vals_train = train_data['type_x'].unique()
+type_x_vals_test = test_data['type_x'].unique()
 
-# test_data["unemploymant rate '95 "] = pd.to_numeric(test_data["unemploymant rate '95 "], errors='coerce')
-# test_data["no. of commited crimes '95 "] = pd.to_numeric(test_data["no. of commited crimes '95 "], errors='coerce')
-# test_data["unemploymant rate '95 "] = pd.to_numeric(test_data["unemploymant rate '95 "], errors='coerce')
-# test_data["no. of commited crimes '95 "] = pd.to_numeric(test_data["no. of commited crimes '95 "], errors='coerce')
+type_x_vals = list(type_x_vals_train)
+type_x_vals.extend(x for x in type_x_vals_test if x not in type_x_vals)
 
+train_data = pd.get_dummies(train_data, columns=['type_x'], dtype=bool)
+test_data = pd.get_dummies(test_data, columns=['type_x'], dtype=bool)
 
-# Converting text columns to boolean columns
+for col in type_x_vals:
+    if col not in train_data.columns:
+        train_data["type_x_" + col] = False
+    elif col not in test_data.columns:
+        test_data["type_x_" + col] = False
+
+train_data["k_symbol"].fillna('no info', inplace=True)
+test_data["k_symbol"].fillna('no info', inplace=True)
+
+for index, row in train_data.iterrows():
+    if train_data.loc[index, 'k_symbol'] == ' ':
+        train_data.loc[index, 'k_symbol'] = 'no info'
+for index, row in test_data.iterrows():
+    if test_data.loc[index, 'k_symbol'] == ' ':
+        test_data.loc[index, 'k_symbol'] = 'no info'
+        
+k_symbol_vals_train = train_data['k_symbol'].unique()
+k_symbol_vals_test = test_data['k_symbol'].unique()
+
+k_symbol_vals = list(k_symbol_vals_train)
+k_symbol_vals.extend(x for x in k_symbol_vals_test if x not in k_symbol_vals)
+
+train_data = pd.get_dummies(train_data, columns=['k_symbol'], dtype=bool)
+test_data = pd.get_dummies(test_data, columns=['k_symbol'], dtype=bool)
+
+for col in k_symbol_vals:
+    if col not in train_data.columns:
+        train_data["k_symbol_" + col] = False
+    elif col not in test_data.columns:
+        test_data["k_symbol_" + col] = False  
 
 train_data = pd.get_dummies(train_data, columns=['frequency'], dtype=bool)
 test_data = pd.get_dummies(test_data, columns=['frequency'], dtype=bool)
 
-train_data = pd.get_dummies(train_data, columns=['type_y'], dtype=bool)
-test_data = pd.get_dummies(test_data, columns=['type_y'], dtype=bool)
-
-test_data = test_data.drop_duplicates(subset=['loan_id'], keep='first')
-
-# train_data["unemploymant rate '95 "].fillna((train_data["unemploymant rate '95 "].mean()), inplace=True)
-# train_data["no. of commited crimes '95 "].fillna((train_data["no. of commited crimes '95 "].mean()), inplace=True)
-
-# test_data["unemploymant rate '95 "].fillna((test_data["unemploymant rate '95 "].mean()), inplace=True)
-# test_data["no. of commited crimes '95 "].fillna((test_data["no. of commited crimes '95 "].mean()), inplace=True)
-
 train_data = train_data.dropna()
 competition_inputs = test_data.drop(columns=["loan_id", "status"])
 test_data = test_data.drop(columns=["status"])
+test_data = test_data.dropna() # test_data must always have 354 rows, keeping it there to make sure
 all_ids_comp = test_data['loan_id'].values
 
-# corr_matrix = train_data.corr()
-# heatMap=sb.heatmap(corr_matrix, annot=True,  cmap="YlGnBu", annot_kws={'size':12})
-# heatmap=plt.gcf()
-# heatmap.set_size_inches(20,15)
-# plt.xticks(fontsize=14)
-# plt.yticks(fontsize=14)
+default_data = train_data.loc[train_data['status'] == -1]
+LIMIT = len(default_data)
+non_default_data = train_data.loc[train_data['status'] == 1]
+non_default_data = non_default_data[:LIMIT]
 
-all_inputs = train_data.drop(columns=["loan_id"])
+train_data = pd.concat([default_data, non_default_data])
+
+all_inputs = train_data.drop(columns=["status", "loan_id"])
 all_labels = train_data['status'].values
 all_ids = train_data['loan_id'].values
 
@@ -124,58 +136,52 @@ all_ids_comp = test_data['loan_id'].values
 
 X_train, X_test, y_train, y_test = train_test_split(all_inputs, all_labels, test_size=0.25)
 
-y_train = pd.DataFrame(y_train, columns=['status'])
-undersampled = pd.concat([X_train, y_train])
+y_train_temp = pd.DataFrame(y_train, columns=['status'])
+X_train.reset_index(drop=True, inplace=True)
+y_train_temp.reset_index(drop=True, inplace=True)
+all_train_data = pd.concat([X_train, y_train_temp], axis=1, join='outer')
 
-
-default_data = X_train.loc[X_train['status'] == -1]
+default_data = all_train_data.loc[all_train_data['status'] == -1]
 LIMIT = len(default_data)
-non_default_data = X_train.loc[X_train['status'] == 1]
+non_default_data = all_train_data.loc[all_train_data['status'] == 1]
 non_default_data = non_default_data[:LIMIT]
 
-X_train = pd.concat([default_data, non_default_data])
-y_train = X_train['status'].values
-X_train = X_train.drop(columns=["status"])
-
-X_test = X_test.drop(columns=["status"])
+all_train_data = pd.concat([default_data, non_default_data])
 
 scaler = StandardScaler()
 scaler.fit(X_train)
 
 X_train = scaler.fit_transform(X_train)
-
 X_test = scaler.fit_transform(X_test)
 competition_inputs = scaler.fit_transform(competition_inputs)
 
-cross_validation = StratifiedKFold(n_splits=10)
+dt_classifier = AdaBoostClassifier(random_state=1)
 
-# SVM 
-svm_classifier = SVC(random_state=1, probability=True)
+dt_grid_search = GridSearchCV(dt_classifier,
+                            param_grid={},
+                            scoring='roc_auc',
+                            cv=10)
 
-parameters = [{'kernel': ['rbf', 'linear'], 'gamma': [1e-3, 1e-4], 'C': [0.01, 0.1, 1, 10, 100]}]
+dt_grid_search.fit(X_train, y_train)
+best_score = dt_grid_search.best_score_
+print("Best Score: " + str(best_score))
 
-scoring = {"AUC": "roc_auc", "Accuracy": make_scorer(accuracy_score)}
+predictions_train = dt_grid_search.predict(X_train)
+predictions_test = dt_grid_search.predict(X_test)
 
-grid_search = GridSearchCV(svm_classifier,
-                    parameters,
-                    n_jobs=4,
-                    scoring='roc_auc',
-                    refit="AUC",
-                    return_train_score=True,    
-                    cv=cross_validation)
 
-grid_search.fit(X_train, y_train)
+print('Best score: {}'.format(dt_grid_search.best_score_))
+print('Best parameters: {}'.format(dt_grid_search.best_params_))
+#cv_scores = cross_val_score(svm_classifier, all_inputs, all_labels, cv=cross_validation, scoring="roc_auc")
+#print('Mean ROC AUC: %.3f' % np.mean(cv_scores))
 
-predictions_train = grid_search.predict(X_train)
-predictions_test = grid_search.predict(X_test)
-
-predictions_competition = grid_search.predict_proba(competition_inputs)
-predictions_competition = pd.DataFrame(predictions_competition, columns=['Predicted', 'col2'])
+predictions_competition = dt_grid_search.predict_proba(competition_inputs)
+predictions_competition = pd.DataFrame(predictions_competition, columns=['Predicted1', 'col2'])
 predictions_competition.drop('col2', axis=1, inplace=True)
 dataframetemp = pd.DataFrame(all_ids_comp, columns=['Id'])
 dataframeids = pd.concat([dataframetemp, predictions_competition], axis=1)
 results = dataframeids.drop_duplicates(subset=['Id'], keep='first')
 
-print("auc: {}".format(grid_search.best_score_))
+results = results.merge(good, on='Id')
 
 results.to_csv('out.csv', index = False)
